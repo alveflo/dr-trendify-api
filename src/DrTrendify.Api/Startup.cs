b8baed55f1b0b8454eefaf330af1d2db06619002
alveflo <victor.alveflo@gmail.com>
2020-02-17 18:42:15 +0100

    Trigger build

--- src/DrTrendify.Api/Startup.cs
+++ src/DrTrendify.Api/Startup.cs
@@ -49,8 +49,10 @@ namespace DrTrendify.Api
 
             var configCatConfig = new ConfigCatConfig();
             Configuration.GetSection("CatConfig").Bind(configCatConfig);
+
             Console.WriteLine($"Config cat api key: {configCatConfig.Key}");
             Console.WriteLine($"Redis api key: {redisConfig.ConnectionString}");
+            Console.WriteLine($"November scraper base url: {Configuration["NovemberScraperConfig:BaseUrl"]}");
 
             services.AddConfigCat(configCatConfig);
 
